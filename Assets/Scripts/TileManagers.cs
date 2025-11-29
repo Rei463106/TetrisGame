7e05733d5d4785cb73ae638264273d4efb395d57
mayarei2662 <mayarei2662@gmail.com>
2025-11-29 10:26:17 +0900

    進まない

---
+++ Assets/Scripts/TileManagers.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManagers : MonoBehaviour
+{
+    //タイルの位置を入れるための配列
+    [SerializeField] int height;
+    [SerializeField] int width;
+    
+
+}
